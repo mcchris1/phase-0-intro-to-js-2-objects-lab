3b65f6a2968328299a69faebaccf43dda88ff3c1
mcchris1 <121463561+mcchris1@users.noreply.github.com>
2023-01-05 16:03:49 -0500

    commit tree 2f933b08eee30a42ff27a042812e8f7ce424c448

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+//Initialize employee (obj) keys (name) + (address) w corresponding values:
+const employee = {
+    name: "Michael", address: "11 Broadway"
+}
+//functions
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = Object.assign({}, employee, {[key]:value});
+    return newEmployee;
+}
